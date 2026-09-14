ab14ddf91cf87cb1584a819378f9f1e92718cac0
gabrielcarvalho <gabriellllcarvalho11@gmail.com>
2026-09-13 23:26:11 -0300

    Código da aula 31 de agosto

--- src/main/java/Oi.java
+++ src/main/java/Oi.java
@@ -1,6 +1,26 @@
+import javax.swing.JOptionPane;
+
 public class Oi {
-    public static void main (String [] args){
-        System.out.print("Oi");
+
+    public static void mostreMensagemBoasVindas(String nome){
+        System.out.println("Oi "+ nome);
+        System.out.println("Como vai você?");
+    }
+
+
+    public static void main(String [] args){
+        String nomeLido = JOptionPane.showInputDialog("Diga seu nome");
+        mostreMensagemBoasVindas(nomeLido);
+        int x = 10;
+        String nome = "Gabriel";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.charAt(6));
+
+
+        char letra = 'c';
+        double altura = 1.75;
+        float numeroReal = 8.87f;
+        boolean toComFome = true;
     }
 
 }
